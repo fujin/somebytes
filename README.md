5aaf237e9f38fdc6d3cee4be7d51532d22f27c9f
AJ Christensen <aj@junglistheavy.industries>
2019-04-12 17:15:08 +1200

    Finish up docs

--- README.md
+++ README.md
@@ -45,11 +45,11 @@ BSD				  Feb 2, 2018				   BSD
 
 Hi! If you are reading this, it means you are reviewing my attempt at the Scytale somebytes takehome test. I appreciate the opportunity.
 
-I have implemented the application as specified with Go. I decided to use the [Go
-Cloud Development kit](https://gocloud.dev/) such that the code would be portable without any additional work between
-AWS S3, GCP and Azure, in-memory and file-blacked blob storage. The facilities should readily support unit, integration, end-to-end testing.
+I have implemented the application as specified with the Go language. I decided to use the [Go
+Cloud Development kit](https://gocloud.dev/) such that the code would be portable without much additional work between
+AWS S3, GCP and Azure, in-memory and file-blacked blob storage. The facilities should readily support unit, integration, end-to-end testing. The recommended way to support multiple providers is through the new 'wire' dependency injection system, which I opted not to make use of, although would be possible to integrate later if multi-provider capabilities became necessary.
 
-For the logger, I selected ripzap, as it's by far the highest performing and has zero allocations - although this is obviously not much of a concern for a tool like this.
+For the logger, I selected ripzap, as it's by far the highest performing and has zero allocations or reflection - although this is obviously not much of a concern for a tool like this.
 
 I've included an 'internal'-type package named blobber, which handles interacting with the blob storage (through the go CDK), and loremipsum which takes care of the specified random number of characters from the Lorem ipsum text.
 
@@ -70,9 +70,28 @@ AWS_REGION
 ```
 
 Despite the general recommendation against doing so, in the interest of the test working, I have included credentials for a bucket in ap-southeast-2. Feel free to substitute your own. I will disable these credentials once everyone has had a chance to review the submission.
+```
+export AWS_ACCESS_KEY_ID=AKIAZKGCQ7DUMSC3QZUU AWS_SECRET_ACCESS_KEY=cK5CkvPWw8Uxal/utndILL1cdpV5EGGzQUJq1xss AWS_REGION=ap-southeast-2
+```
+
+I suggest erasing the keys from the bucket prior to running the test e.g. with AWS CLI; running the test binary in the 'list' mode, to confirm the bucket is empty, followed by the 'create' mode, to create keys up to the limit, then finally the test binary in the 'list' mode again.
+
+## Linting
+The code was developed against the standards checked by [golangci-lint](https://golangci.com/)
+
+To install, run:
+```
+# binary will be $(go env GOPATH)/bin/golangci-lint
+curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin latest
+```
+
+Then invoke in the root of the project:
+```
+golangci-lint run
+```
 
 ## Testing
-Extensive unit test coverage is included. The surface is quite small, and dependencies extensively unit tested/benchmarked.
+Extensive unit test coverage is included. The surface is quite small with dependencies extensively unit tested/benchmarked.
 
 ### Unit
 ```
--- internal/blobber/blobber.go
+++ internal/blobber/blobber.go
@@ -11,45 +11,57 @@ import (
 	"gocloud.dev/blob"
 )
 
+// blobBucketWriter represents the internal interface between upstream *blob.Bucket#NewWriter().
 type blobBucketWriter interface {
 	NewWriter(ctx context.Context, key string) (io.WriteCloser, error)
 }
 
-type listIterator interface {
-	Next(context.Context) (*blob.ListObject, error)
-}
-
+// blobBucketLister represents the internal interface between upstream *blob.Bucket#List().
 type blobBucketLister interface {
 	List() listIterator
 }
 
+// listIterator represents the internal interface between upstream *blob.ListIterator#Next()
+type listIterator interface {
+	Next(context.Context) (*blob.ListObject, error)
+}
+
+// blobBucket is a convenience interface
 type blobBucket interface {
 	blobBucketWriter
 	blobBucketLister
 }
 
+// BlobBucket represents the public interface of the upstream *blob.Bucket
 type BlobBucket interface {
 	List(*blob.ListOptions) *blob.ListIterator
 	NewWriter(context.Context, string, *blob.WriterOptions) (*blob.Writer, error)
 }
 
+// blobBucketContainer is an internal structure which allows us to decorate/delegate method calls to the upstream implementation.
 type blobBucketContainer struct {
 	blobBucket BlobBucket
 }
 
+// NewWriter is our internal decorated method of upstream NewWriter.
 func (b blobBucketContainer) NewWriter(ctx context.Context, key string) (io.WriteCloser, error) {
 	return b.blobBucket.NewWriter(ctx, key, nil)
 }
 
+// List is our internal decorated method of upstream List().
 func (b blobBucketContainer) List() listIterator {
 	return b.blobBucket.List(nil)
 }
 
+// Blobber allows us to create an encapsulated *blob.Bucket, while carefully
+// swapping out some of its fields, to allow for testing. The public interfaces
+// do not easily allow for this.
 type Blobber struct {
 	generator  func() ([]byte, error)
 	blobBucket blobBucket
 }
 
+// New creates a Blobber object with error checking for the string generator, and the upstream *blob.Bucket.
 func New(generator func() ([]byte, error), bb BlobBucket) (*Blobber, error) {
 	if generator == nil {
 		return nil, errors.New("generator function cannot be nil")
@@ -63,6 +75,10 @@ func New(generator func() ([]byte, error), bb BlobBucket) (*Blobber, error) {
 	}, nil
 }
 
+// CreateObjects instructs the Blobber to seed PRNG and create individual
+// randomized keys in the bucket, up to the limit. Upstream errors are wrapped
+// for the call-site to handle. Additionally, the expected number of bytes
+// written is checked.
 func (b *Blobber) CreateObjects(ctx context.Context, limit int) error {
 	// Seed the pseudo-random number generator.
 	rand.Seed(time.Now().UnixNano())
@@ -97,11 +113,16 @@ func (b *Blobber) CreateObjects(ctx context.Context, limit int) error {
 	return nil
 }
 
+// Object is our internal version of objects returned from ListObjects. It's
+// used primarily for display and testing purposes.
 type Object struct {
 	Key  string
 	Size int64
 }
 
+// ListObjects instructs the Blobber to query the bucket for objects, followed
+// by determining if said object is greater than the threshold (in bytes). A
+// slice of internal Object types is returned for display purposes.
 func (b *Blobber) ListObjects(ctx context.Context, threshold int) ([]Object, error) {
 	var objects []Object
 	iter := b.blobBucket.List()
