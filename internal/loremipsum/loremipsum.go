f936d7db05df3ebe78d4d91bb7cf7618c3e4b917
AJ Christensen <aj@junglistheavy.industries>
2019-04-12 12:49:15 +1200

    More tests.

--- internal/loremipsum/loremipsum.go
+++ internal/loremipsum/loremipsum.go
@@ -7,27 +7,28 @@ import (
 
 var (
 	// The canonical Lorem ipsum.
-	Phrase = `
+	phrase = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 `
-	// Convert the phrase (string) into a slice of runes (characters)
-	Chars = []rune(Phrase)
+	// Convert the phrase (string) into a rune-slice (characters)
+	chars = []rune(phrase)
 )
 
 const (
+	// Limit the byte slice to a maximum potential length.
 	maximumRandomCharacters = 1e6
 )
 
-// "A random number of characters of Lorem Ipsum" Characters are
-// kind of tricky in Go. We have to assume the test writer was
-// referring to a UTF-8 character, e.g. potentially multi-byte.
+// "A random number of characters of Lorem Ipsum" Characters are kind of tricky
+// in Go. We have to assume the test writer was referring to a UTF-8 character,
+// e.g. potentially multi-byte, although there are none in the phrase..
 func RandomCharacters() []byte {
 	// Make a string builder to randomly write Lorem Ipsum's runes into.
 	var builder strings.Builder
 
 	// Iterate up to a random length, lower than the maximum.
 	for c := 0; c < rand.Intn(maximumRandomCharacters); c++ {
-		builder.WriteRune(Chars[rand.Intn(len(Chars))])
+		builder.WriteRune(chars[rand.Intn(len(chars))])
 	}
 
 	// Turn the string builder into byte slice, since we need one of those for writing later.
--- internal/loremipsum/loremipsum_test.go
+++ internal/loremipsum/loremipsum_test.go
@@ -7,14 +7,24 @@ import (
 
 // This is not a useful test.
 func TestChars(t *testing.T) {
-	if len(Chars) != 447 {
+	if len(chars) != 447 {
 		t.Fatal("Lorem ipsum rune slice did not have expected length.")
 	}
 }
 
 // Also not great.
 func TestPhrase(t *testing.T) {
-	if !strings.Contains(Phrase, "Lorem ipsum") {
+	if !strings.Contains(phrase, "Lorem ipsum") {
 		t.Fatal("Phrase did not contain expected string")
+	}
+}
+
+func TestRandomCharacters(t *testing.T) {
+	if string(RandomCharacters()) == phrase {
+		t.Fatal("Random Characters was the phrase! That's not random at all.")
+	}
+
+	if len(RandomCharacters()) > maximumRandomCharacters {
+		t.Fatal("Random characters over the maximum limit")
 	}
 }
